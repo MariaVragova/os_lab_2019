6cdb85045f0593712f807a6519db3681955e6978
mariavragova <masha.vragova@gmail.com>
2021-02-24 18:29:45 +0000

    average and numbers

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-echo $# parameters
+#echo $# parameters
+FILE=$1
+parameters=0
 sum=0
-for params in "$@"
+#for params in "$@";
+while read LINE;
 do
-sum=$(($params+$sum))
-done
-average=$(($sum/$#))
-echo $average
+sum=$(($LINE+$sum))
+parameters=$(($parameters+1))
+echo parameter [$parameters] - "$LINE";
+#echo $params
+#sum=$(($params+$sum))
+#echo $sum;
+#done < numbers.txt
+done < $FILE
+echo $parameters- parameters
+average=$(($sum/$parameters))
+echo $average - average
 
 
