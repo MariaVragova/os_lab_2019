cac67b9e2f4c3321b144a58c97465cbccf904139
mariavragova <masha.vragova@gmail.com>
2021-02-25 09:03:08 +0000

    добаÐrevert_stringc

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,27 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    // your code here
+    if (str == 0 || *str == 0)
+    {
+        return;
+    }
+
+    char* start = str;
+    char* end = start + strlen(str) - 1;
+    char temp;
+
+    while (end > start)
+    {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        ++start;
+        --end;
+    }
 }
 
