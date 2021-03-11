ef05101a2b032a9545063e8658dae0417548fa71
hamster-21 <masha.vragova@gmail.com>
2021-03-11 11:46:05 +0000

    исправлен parallel_min_max.c

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -42,16 +42,34 @@ int main(int argc, char **argv) {
             seed = atoi(optarg);
             // your code here
             // error handling
+
+            if (seed <= 0) {
+             printf("seed is a positive number\n");
+             return 1;
+            }
+
             break;
           case 1:
             array_size = atoi(optarg);
             // your code here
             // error handling
+
+            if (array_size <= 0) {
+             printf("array_size is a positive number\n");
+             return 1;
+            }
+
             break;
           case 2:
             pnum = atoi(optarg);
             // your code here
             // error handling
+
+            if (pnum < 1) {
+             printf("pnum => 1 \n");
+             return 1;
+            }
+
             break;
           case 3:
             with_files = true;
